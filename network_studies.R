ba9bccac91aab8396c96b537b07ae8fc3433ec8a
tamas <tamas@localhost.localdomain>
2020-09-21 13:05:20 +0200

    network functions extended

--- network_studies.R
+++ network_studies.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(tibble)
 
 
 # functions
@@ -7,20 +8,20 @@ estimate_k <- function(W, L, C, V) {
   # k is the optimal distance between vertices; it's the radius of the empty area around a vertex
   # constant C is found experimentally
 
-  area <- W * L 
+  area <- W * L
   n_vertices  <- length(V)
   C * sqrt(area / n_vertices)
 }
 
 # force declarations; where d is the distance between two vertices
 #attractve
-force_attract <- function(d, k){
-  d^2 / k
+force_attract <- function(d, k, params = "d^2 / k"){
+  eval(parse(text = params))
 }
 
 #repulsive
-force_repulsive <- function(d, k) {
-  (-1) * (k^2 / d)
+force_repulsive <- function(d, k, params = "(-1) * (k ^2 / d)") {
+  eval(parse(text = params))
 }
 
 
@@ -45,3 +46,14 @@ ggplot(res, aes(x = distance), alpha = 0/5, size = 4)+
     geom_text(aes(x = zero_d, y = -1, label = "k, the ideal distance of vertices \n where forces cancel each other"), hjust = -0.25, vjust= 0.75) +
     ylim(-251,250)
 
+test_input <- tibble(
+         "vertex_a" = c("a", "b", "c"),
+         "vertex_b" = c("b", "c", "a"),
+         "strength" = c(5, 4, 3)
+  )
+
+network_graph <- function(input) {
+  W  <- seq(1, ncol(input) * 100)
+  L  <- seq(1, ncol(input) * 100)
+  area  <-W * L
+}
